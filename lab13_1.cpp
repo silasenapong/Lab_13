2ae73a7e7f234aabc606ed4db0504005be3fc24f
silasenapong <sila_sanapong@cmu.ac.th>
2026-01-06 15:05:38 +0700

    add name

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -3,13 +3,14 @@
 #include <cmath>
 using namespace std;
 
-void stat(const double[],int,double[]);
+void stat(const double[], int, double[]);
 
-int main(){
-    double A[] = {1.2,3.5,6.9,7.8,12.5,0.5};
-    int N = sizeof(A)/sizeof(A[0]);
+int main()
+{
+    double A[] = {1.2, 3.5, 6.9, 7.8, 12.5, 0.5};
+    int N = sizeof(A) / sizeof(A[0]);
     double B[6];
-    stat(A,N,B);
+    stat(A, N, B);
     cout << fixed << setprecision(2);
     cout << "Arithmetic Mean = " << B[0] << endl;
     cout << "Standard Deviation = " << B[1] << endl;
--- lab13_2.cpp
+++ lab13_2.cpp
@@ -5,19 +5,20 @@ using namespace std;
 const int N = 30;
 const int M = 70;
 
-void updateImage(bool [][M],int,int,int);
+void updateImage(bool[][M], int, int, int);
 
-void showImage(const bool [][M]);
+void showImage(const bool[][M]);
 
 int main()
 {
     bool image[N][M] = {};
-    int s,x,y;
-    do{
+    int s, x, y;
+    do
+    {
         showImage(image);
         cout << "Input your brush size and location: ";
         cin >> s >> x >> y;
-        updateImage(image,s,x,y);
-    }while(s != 0 || x != 0 || y != 0);
+        updateImage(image, s, x, y);
+    } while (s != 0 || x != 0 || y != 0);
     return 0;
 }
